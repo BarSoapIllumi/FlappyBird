947f3b01758f6badc59188210636c43dde8ea7a4
Angela <angela.zhou12345@gmail.com>
2024-10-09 02:43:22 -0400

    In repo

---
+++ C1.c
@@ -0,0 +1,32 @@
+# Write your code here :-)
+q1 = input("whats my name? ")
+if q1 == "angela":
+    print("yipee")
+else:
+    print("nuh uh")
+    
+q2 = input("whats my cat's name? ")
+if q2 == "illumi":
+    print("yipee")
+else:
+    print("nuh uh")
+    
+q3 = input("whats my favorite food? ")
+if q3 == "sushi":
+    print("yipee")
+else:
+    print("nuh uh")
+    
+q4 = input("whos my favorite person? ")
+if q4 == "mom":
+    print("yipee")
+else:
+    print("nuh uh")
+    
+q5 = input("what day is my bday? ")
+q5 = int(q5)
+if q5 == 18:
+    print("yipee")
+else:
+    print("nuh uh")
+    
